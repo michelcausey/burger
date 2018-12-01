30356fd1e98612ae65143c8d261b57c6a849b3a3
MiChel Causey <michel.causey@gmail.com>
2018-11-30 21:39:38 -0500

    boiler plate, i think

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,69 @@
 // Express
 // burger.js
 
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger");
+
 // Create the router for the app, and export the router at the end of your file.
+// selectAll()
+// insertOne()
+// updateOne()
+
+// selectAll()
+router.get("/", function(req, res) {
+    // burger.all(function(data) {
+    //   var hbsObject = {
+    //     cats: data
+    //   };
+    //   console.log(hbsObject);
+    //   res.render("index", hbsObject);
+    // });
+  });
+  
+// insertOne()
+  router.post("/api/burger", function(req, res) {
+  // burger.create([
+  //    "name", "sleepy"
+  //  ], [
+  //     req.body.name, req.body.sleepy
+  //  ], function(result) {
+  // Send back the ID of the new quote
+  //    res.json({ id: result.insertId });
+  //  });
+  });
+  
+  router.put("/api/cats/:id", function(req, res) {
+    // var condition = "id = " + req.params.id;
+  
+    // console.log("condition", condition);
+  
+    // burger.update({
+    //   sleepy: req.body.sleepy
+    // }, condition, function(result) {
+    //   if (result.changedRows == 0) {
+    //     // If no rows were changed, then the ID must not exist, so 404
+    //     return res.status(404).end();
+    //   } else {
+    //     res.status(200).end();
+    //   }
+    // });
+  });
+  
+  router.delete("/api/burger/:id", function(req, res) {
+    // var condition = "id = " + req.params.id;
+  
+    // burger.delete(condition, function(result) {
+    //   if (result.affectedRows == 0) {
+    //     // If no rows were changed, then the ID must not exist, so 404
+    //     return res.status(404).end();
+    //   } else {
+    //     res.status(200).end();
+    //   }
+    // });
+  });
+  
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,32 @@
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
 // Export at the end of the burger.js file.
+
+var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burger", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
